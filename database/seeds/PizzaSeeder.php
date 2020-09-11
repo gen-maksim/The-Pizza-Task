2bfcdd4ea8897ccf5d80dcc2b61dabc7b37975ff
Генько Максим <gen-maksim@yandex.ru>
2020-09-11 21:25:01 +0300

    add pizza seeder

---
+++ database/seeds/PizzaSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Pizza;
+use Carbon\Carbon;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class PizzaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+        for ($i = 1; $i <= 8; $i++) {
+            Pizza::create([
+                'name' => ($i < 8 ? Carbon::now()->addDays($i)->englishDayOfWeek : 'Special') . ' pizza',
+                'description' => $faker->realText(100),
+                'cost' => $faker->numberBetween(20, 50),
+                'picture' => $i,
+            ]);
+        }
+    }
+}
