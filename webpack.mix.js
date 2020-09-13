97d38822cde7b08bbaa9ccf545bd2608abd9bc9d
Генько Максим <gen-maksim@yandex.ru>
2020-09-13 23:50:48 +0300

    add postinstall to package

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/assets/js')
-    .sass('resources/sass/app.scss', 'public/assets/css');
+mix.js('resources/js/app.js', 'public/assets')
+    .sass('resources/sass/app.scss', 'public/assets');
